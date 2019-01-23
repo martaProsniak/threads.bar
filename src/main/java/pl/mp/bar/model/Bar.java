9ac7d53038771a6ccdbdb8f72a51e433b4d42202
martaprosniak <martaprosniak@gmail.com>
2019-01-23 18:52:52 +0100

    Bar tests

--- src/main/java/pl/mp/bar/model/Bar.java
+++ src/main/java/pl/mp/bar/model/Bar.java
@@ -1,25 +1,45 @@
 package pl.mp.bar.model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Bar representation.
  */
 public class Bar {
+
+    private static final Logger log = Logger.getLogger(Bar.class.getCanonicalName());
     /**
      * Drink from the bar.
      */
     private String drink;
 
+    public String getDrink() {
+        return drink;
+    }
 
-    public synchronized void takeDrink() throws InterruptedException{
-        while (drink.equals(null)){
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    private String[] drinks = {"Thunderbolt", "Blizzard", "Swallow", "Cat", "Black blood"};
+
+    public synchronized void takeDrink() throws InterruptedException {
+        while (drink.equals(null)) {
             wait();
         }
         this.drink = null;
     }
 
-    public synchronized String putDrink(String drink) {
-        this.drink = drink;
-        notifyAll();
-        return drink;
+    public synchronized void putDrink() {
+        for (int i = 0; i < drinks.length; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                log.log(Level.WARNING, ex.getMessage(), ex);
+            }
+            this.drink = drinks[i];
+            notifyAll();
+        }
     }
 }
--- src/main/java/pl/mp/bar/model/Barman.java
+++ src/main/java/pl/mp/bar/model/Barman.java
@@ -1,9 +1,14 @@
 package pl.mp.bar.model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Barman representation.
  */
 public class Barman implements Runnable {
+
+    public static final Logger log = Logger.getLogger(Barman.class.getCanonicalName());
     /**
      * Barman's bar.
      */
@@ -15,7 +20,12 @@ public class Barman implements Runnable {
 
     public void run() {
         while (!Thread.interrupted()) {
-            String drink = bar.putDrink("Pina Colada");
+            try {
+                bar.putDrink();
+                Thread.sleep(1000);
+            } catch (InterruptedException ex){
+                log.log(Level.WARNING, ex.getMessage(), ex);
+            }
         }
     }
 
